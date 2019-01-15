c1aa7844a2ff05aaabff895c21ac3b7837eec098
Raymond Médédé KPATCHAA <kraymond70@gmail.com>
2019-01-15 17:12:11 +0100

    I should import Button right ?

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 
 import request from 'axios';
 
